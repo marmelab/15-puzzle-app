685974584912f0468ca9f5ec27083e6f88ccf9f4
Luwangel <adrien.amoros@gmail.com>
2017-11-20 14:49:45 +0100

    Fix the linter

--- src/services/GameService.js
+++ src/services/GameService.js
@@ -7,7 +7,7 @@ const DEFAULT_HEADER_JSON = {
 
 const DEFAULT_BASE_URL = config.apiUrl;
 
-export default newGame = (baseUrl = DEFAULT_BASE_URL) => (mode = 'multi') => {
+export const newGame = (baseUrl = DEFAULT_BASE_URL) => (mode = 'multi') => {
   const url = `${this.baseUrl}/game`;
   const method = 'POST';
   const headers = Object.assign({}, DEFAULT_HEADER_JSON);
@@ -22,7 +22,7 @@ export default newGame = (baseUrl = DEFAULT_BASE_URL) => (mode = 'multi') => {
   .then(res => res.json());
 };
 
-export default game = (baseUrl = DEFAULT_BASE_URL) => (id, token) => {
+export const game = (baseUrl = DEFAULT_BASE_URL) => (id, token) => {
   const url = `${this.baseUrl}/game/${id}`;
   const method = 'GET';
   const headers = Object.assign({}, DEFAULT_HEADER_JSON, {
@@ -36,7 +36,7 @@ export default game = (baseUrl = DEFAULT_BASE_URL) => (id, token) => {
   .then(res => res.json());
 };
 
-export default move = (baseUrl = DEFAULT_BASE_URL) => (id, tile, token) => {
+export const move = (baseUrl = DEFAULT_BASE_URL) => (id, tile, token) => {
   const url = `${this.baseUrl}/game/${id}/move/${tile}`;
   const method = 'PUT';
   const headers = Object.assign({}, DEFAULT_HEADER_JSON, {
