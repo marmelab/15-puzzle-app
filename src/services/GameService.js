47a02999d1f671d0ad26a5f2da7102fd4321b520
Luwangel <adrien.amoros@gmail.com>
2017-11-20 13:54:14 +0100

    Export multiple factories instead of a game service class

--- src/services/GameService.js
+++ src/services/GameService.js
@@ -1,77 +1,51 @@
+import config from '../config';
+
 const DEFAULT_HEADER_JSON = {
   'Accept': 'application/json',
   'Content-Type': 'application/json',
 };
 
-/**
- * @class GameService
- */
-export default class GameService {
-  construct(baseUrl) {
-    this.baseUrl = baseUrl;
-  }
+const DEFAULT_BASE_URL = config.apiUrl;
 
-  /**
-   * @method new
-   * @param {string} mode
-   * @description Create a new game
-   * @returns {Promise}
-   */
-  new(mode = 'multi') {
-    const url = `${this.baseUrl}/game`;
-    const method = 'POST';
-    const headers = Object.assign({}, DEFAULT_HEADER_JSON);
+export default newGame = (baseUrl = DEFAULT_BASE_URL) => (mode = 'multi') => {
+  const url = `${this.baseUrl}/game`;
+  const method = 'POST';
+  const headers = Object.assign({}, DEFAULT_HEADER_JSON);
 
-    return fetch(url, {
-      method,
-      headers,
-      body: JSON.stringify({
-        mode
-      })
+  return fetch(url, {
+    method,
+    headers,
+    body: JSON.stringify({
+      mode
     })
-    .then(res => res.json());
-  }
+  })
+  .then(res => res.json());
+};
 
-  /**
-   * @method new
-   * @param {int} id The game's id
-   * @param {string} token An authorization token
-   * @description Get a game based on it's id
-   * @returns {Promise}
-   */
-  game(id, token) {
-    const url = `${this.baseUrl}/game/${id}`;
-    const method = 'GET';
-    const headers = Object.assign({}, DEFAULT_HEADER_JSON, {
-      Authorization: `Bearer  ${token}`
-    });
+export default game = (baseUrl = DEFAULT_BASE_URL) => (id, token) => {
+  const url = `${this.baseUrl}/game/${id}`;
+  const method = 'GET';
+  const headers = Object.assign({}, DEFAULT_HEADER_JSON, {
+    Authorization: `Bearer  ${token}`
+  });
 
-    return fetch(url, {
-      method,
-      headers: HEADER_JSON
-    })
-    .then(res => res.json());
-  }
+  return fetch(url, {
+    method,
+    headers: HEADER_JSON
+  })
+  .then(res => res.json());
+};
 
-  /**
-   * @method new
-   * @param {int} id The game's id
-   * @param {int} tile The tile number
-   * @param {string} token An authorization token
-   * @description Move a tile from a game
-   * @returns {Promise}
-   */
-  move(id, tile, token) {
-    const url = `${this.baseUrl}/game/${id}/move/${tile}`;
-    const method = 'PUT';
-    const headers = Object.assign({}, DEFAULT_HEADER_JSON, {
-      Authorization: `Bearer  ${token}`
-    });
+export default move = (baseUrl = DEFAULT_BASE_URL) => (id, tile, token) => {
+  const url = `${this.baseUrl}/game/${id}/move/${tile}`;
+  const method = 'PUT';
+  const headers = Object.assign({}, DEFAULT_HEADER_JSON, {
+    Authorization: `Bearer  ${token}`
+  });
 
-    return fetch(url, {
-      method,
-      headers: HEADER_JSON
-    })
-    .then(res => res.json());
-  }
-}
+  return fetch(url, {
+    method,
+    headers: HEADER_JSON
+  })
+  .then(res => res.json());
+};
