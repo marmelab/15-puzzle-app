39cbac81e28a3458ca0cf2f093e7b69bd7ef07a0
Luwangel <adrien.amoros@gmail.com>
2017-11-21 11:01:19 +0100

    Refact the app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import React, { Component } from 'react';
 import { StackNavigator } from 'react-navigation';
 
 import HomeScreen from './routes/HomeScreen';
 import GameScreen from './routes/GameScreen';
 
-const Main = StackNavigator({
+const App = StackNavigator({
     Home: { screen: HomeScreen },
     Game: { screen: GameScreen },
 });
 
-export default class App extends Component {
-    render() {
-        return <Main />;
-    }
-}
+export default App;
