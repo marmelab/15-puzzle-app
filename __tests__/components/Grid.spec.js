a83bf032b652ed4c1ec1b7a949a22fce6dc56f12
Luwangel <adrien.amoros@gmail.com>
2017-11-21 17:40:23 +0100

    Change the tests

--- __tests__/components/Grid.spec.js
+++ __tests__/components/Grid.spec.js
@@ -1,11 +1,10 @@
 import 'react-native';
 import React from 'react';
-
-import Grid from '../src/components/Grid';
+import Grid from '../../src/components/Grid';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('should render Grid correctly', () => {
-    renderer.create(<Grid />);
+    renderer.create(<Grid onPress={() => {}} grid={[]} />);
 });
--- __tests__/components/Tile.spec.js
+++ __tests__/components/Tile.spec.js
@@ -1,10 +1,10 @@
 import 'react-native';
 import React from 'react';
-import Tile from '../src/components/Tile';
+import Tile from '../../src/components/Tile';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('should render Tile correctly', () => {
-    renderer.create(<Tile />);
+    renderer.create(<Tile onPress={() => {}} tileValue={10} />);
 });
