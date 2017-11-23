ac0915642bec104bb46d5ebedc40b5fce184cdda
Luwangel <adrien.amoros@gmail.com>
2017-11-23 13:46:11 +0100

    Add snapshot tests for components

--- __tests__/components/Tile.spec.js
+++ __tests__/components/Tile.spec.js
@@ -8,3 +8,10 @@ import renderer from 'react-test-renderer';
 it('should render Tile correctly', () => {
     renderer.create(<Tile onPress={() => {}} tileValue={10} />);
 });
+
+it('should match snapshot', () => {
+    const tree = renderer
+        .create(<Tile onPress={() => {}} tileValue={12} />)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
